a6ae948eccc0525b927e31b21595c1e40e6d0f02
Nick Goncharenko <hellkennedy@gmail.com>
2025-11-24 19:57:54 +0100

    Initial Commit

---
+++ Testio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Testio
+//
+//  Created by Nick Goncharenko on 24.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
